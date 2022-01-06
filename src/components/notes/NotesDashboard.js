eb40fc1ffaf73e32c1c7c79d8d4051fee1d03de3
inFamousxD <audimankar@gmail.com>
2022-01-06 22:19:48 +0530

    Added base dash for notes

---
+++ src/components/notes/NotesDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FolderDrawer from './FolderDrawer'
+import Note from './Note'
+
+const NotesDashboard = () => {
+	return (
+		<div>
+			<FolderDrawer />
+			<Note />
+		</div>
+	)
+}
+
+export default NotesDashboard
