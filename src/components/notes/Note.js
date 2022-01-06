45d53ebf780c42b863df4fb68eca58f911876774
inFamousxD <audimankar@gmail.com>
2022-01-06 22:25:08 +0530

    Cleanup

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,15 +1,9 @@
 import { Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
-import FolderDrawer from './FolderDrawer';
 
 const Note = () => {
-	const location = useLocation().pathname.split('/');
-	location.shift();
-	console.log(location)
 	return (
 		<React.Fragment>
-			<FolderDrawer />
 			<Card sx={{
 				margin: '2vh 3.5vw 0vh 3.5vw',
 			}}>
@@ -22,10 +16,6 @@ const Note = () => {
 						species, ranging across all continents except Antarctica
 					</Typography>
 				</CardContent>
-				{/* <CardActions>
-					<Button variant='text' size="small">Share</Button>
-					<Button size="small">Learn More</Button>
-				</CardActions> */}
 			</Card>
 		</React.Fragment>
 	)
