67c0f6e08f2c8bc40616d9c71c9f2d8273af3cf8
inFamousxD <audimankar@gmail.com>
2022-01-07 20:19:06 +0530

    Added bottom navigation

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Dashboard from './components/dashboard/Dashboard';
-import Note from './components/notes/Note';
 import Breadcrumb from './components/breadcrumb/Breadcrumb';
+import NotesDashboard from './components/notes/NotesDashboard';
+import Navigation from './components/navigation/Navigation';
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function MyApp() {
+	const [value, setValue] = React.useState('');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
 		<React.Fragment>
 			<Router>
-				<Box sx={{ width: '100vw', height: '7vh', display: 'flex', flexDirection: 'column', bgcolor: '#222', position: 'sticky', top: '0'}}>
+				<Box sx={{ width: '100vw', minHeight: '7vh', display: 'flex', flexDirection: 'column', bgcolor: '#222', position: 'sticky', top: '0'}}>
 					<Breadcrumb />
 				</Box>
-				<Box sx={{ alignItems: 'center', width: '100vw', minHeight: '93vh', display: 'flex', flexDirection: 'column', bgcolor: 'background.default'}}>
+				<Box sx={{ alignItems: 'center', width: '100vw', minHeight: '86vh', display: 'flex', flexDirection: 'column', bgcolor: 'background.default'}}>
 					<Routes>
 						<Route path='/' element={<Dashboard />} />
-						<Route path='/notetest' element={<Note />} />
+						<Route path='/notes' element={<NotesDashboard />} />
 					</Routes>
+				</Box>
+				<Box sx={{ width: '100vw', minHeight: '7vh', display: 'flex', flexDirection: 'column', bgcolor: '#222', position: 'sticky', top: '0'}}>
+					<Navigation value={value} handleChange={handleChange} />
 				</Box>
 			</Router>
 		</React.Fragment>
