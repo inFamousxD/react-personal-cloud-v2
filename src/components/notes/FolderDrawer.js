7a500b5e606f9a3b2a6bd0290d906d4ec4a73376
inFamousxD <audimankar@gmail.com>
2022-01-07 20:20:36 +0530

    Swipable drawer for notes folders

--- src/components/notes/FolderDrawer.js
+++ src/components/notes/FolderDrawer.js
@@ -1,16 +1,38 @@
-import { SwipeableDrawer } from '@mui/material'
+import Folder from '@mui/icons-material/Folder';
+import { Divider, Link, List, ListItem, SwipeableDrawer, Typography } from '@mui/material'
 import React from 'react'
 
 const FolderDrawer = () => {
-	const [open, setOpen] = React.useState(true);
+	const [open, setOpen] = React.useState(false);
 
 	const toggleDrawer = (toggle) => {
+		console.log(toggle)
 		setOpen(toggle);
 	}
+
+	const folders = [
+		'anime',
+		'random',
+		'office'
+	]
+
 	return (
 		<div>
-			<SwipeableDrawer anchor='left' open={open} onClose={() => toggleDrawer(false)} onOpen={() => toggleDrawer(true)}>
-				
+			<SwipeableDrawer anchor='right' open={open} onClose={() => toggleDrawer(false)} onOpen={() => toggleDrawer(true)}>
+				<List sx={{ bgcolor: 'background.paper', minWidth: '70vw', minHeight: '100vh', fontFamily: 'Quicksand' }}>
+					<ListItem sx={{ marginBottom: '2vh' }}>
+						<Typography sx={{ display: 'flex', alignItems: 'center' }} fontFamily={'Quicksand'} color='text.primary'><Folder sx={{mr: 0.5}}></Folder>Folders</Typography>
+					<Divider />
+					</ListItem>
+						
+					{ folders.map((folder, index) => {
+						return <ListItem button key={index}>
+										<Link href={`/#/${folder}`} sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} underline='hover' color='inherit'>
+											{folder}
+										</Link>
+									</ListItem>
+					}) }
+				</List>
 			</SwipeableDrawer>
 		</div>
 	)
