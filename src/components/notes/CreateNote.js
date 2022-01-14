5820c2cbf832f7106b16aa4343c891754ec0d5da
inFamousxD <audimankar@gmail.com>
2022-01-14 19:06:26 +0530

    Added logical note creation method

--- src/components/notes/CreateNote.js
+++ src/components/notes/CreateNote.js
@@ -1,6 +1,10 @@
 import { FavoriteBorder, FavoriteRounded } from '@mui/icons-material'
-import { Button, Card, CardActions, CardContent, IconButton, TextField } from '@mui/material'
+import { Button, Card, CardActions, CardContent, IconButton, TextField, Typography } from '@mui/material'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { db } from '../../firebase/firebase-config';
 
 const CreateNote = () => {
 	const [state, setState] = React.useState({
@@ -8,12 +12,30 @@ const CreateNote = () => {
 		body: '',
 		favourite: false
 	});
+	const location =  useLocation().pathname.split('/');
 
 	const handleStateChange = (event) => {
 		setState({...state, [event.target.name]: event.target.value})
 	}
 
-	console.log(state)
+	const handleCreateNewNote = () => {
+		const folderName = location[location.length-2];
+		const data = {
+			content: {
+				body: state.body,
+				title: state.title,
+				favourite: state.favourite
+			},
+			createdAt: Date.now()
+		}
+
+		const auth = getAuth();
+		onAuthStateChanged(auth, async user => {
+			if (user) {
+				addDoc(collection(db, "users", user.uid, "notes", folderName, "data"), data);
+			}
+		})
+	}
 
 	return (
 		<Card sx={{
@@ -26,6 +48,7 @@ const CreateNote = () => {
 			flexDirection: 'column'
 		}}>
 			<CardContent>
+				<Typography variant='h5'>Create a new Note</Typography>
 				<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
 					<TextField sx={{ mt: 2, width: '80%' }} variant='outlined' fullWidth label='Enter note title' size='small' value={state.title} onChange={handleStateChange} name='title'/>
 					<IconButton sx={{ mt: 2 }} size='small' onClick={() => setState({ ...state, favourite: !state.favourite })}> { state.favourite ? <FavoriteRounded sx={{ color: '#d32f2f' }} /> : <FavoriteBorder sx={{ color: '#d32f2f' }} /> } </IconButton>
@@ -34,7 +57,7 @@ const CreateNote = () => {
 			</CardContent>
 			<CardActions sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
 				<Button sx={{ mt: 4 }} color='error' variant='text'>Back</Button>
-				<Button sx={{ mt: 4 }} color='success' variant='text'>Create</Button>
+				<Button sx={{ mt: 4 }} color='success' variant='text' onClick={handleCreateNewNote}>Create</Button>
 			</CardActions>
 		</Card>
 	)
