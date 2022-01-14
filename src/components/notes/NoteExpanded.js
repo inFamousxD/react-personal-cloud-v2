c9876e0c08891e2862f396a2bf30e350e4a1b690
inFamousxD <audimankar@gmail.com>
2022-01-14 20:03:37 +0530

    Implemented delete note

--- src/components/notes/NoteExpanded.js
+++ src/components/notes/NoteExpanded.js
@@ -1,14 +1,16 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, deleteDoc } from 'firebase/firestore';
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { db } from '../../firebase/firebase-config';
 import Spinner from '../spinner/Spinner';
 import FolderDrawer from './FolderDrawer';
 
 const NoteExpanded = () => {
 	const location = useLocation().pathname.split('/');
+	const navigate = useNavigate();
+
 	const noteId = location[location.length-1];
 	const folderName = location[location.length-2];
 
@@ -24,13 +26,27 @@ const NoteExpanded = () => {
 			setLoading(false);
 		}
 	})
+
+	const deleteNote = () => {
+		onAuthStateChanged(auth, async user => {
+			if (user) {
+				await deleteDoc(doc(db, "users", user.uid, "notes", folderName, "data", noteId));
+				// console.log(docSnap.data());
+				navigate(`/notes/${folderName}`);
+			}
+		})
+	}
+
 	return (
 		!loading ? <div>
 			<FolderDrawer />
 			<Card sx={{
 				width: '93vw',
 				height: '82vh',
-				m: 2
+				m: 2,
+				display: 'flex',
+				justifyContent: 'space-between',
+				flexDirection: 'column'
 			}}>
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="div">
@@ -40,6 +56,9 @@ const NoteExpanded = () => {
 						{data.content.body}
 					</Typography>
 				</CardContent>
+				<CardActions sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+					<Button sx={{ mt: 4 }} color='error' variant='text' onClick={deleteNote}>Delete</Button>
+				</CardActions>
 			</Card>
 		</div> : <Spinner />
 	)
