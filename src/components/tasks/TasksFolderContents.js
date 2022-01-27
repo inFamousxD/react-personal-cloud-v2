3a1c945c92d35c39bd192f90e0e00485c6b74d5e
inFamousxD <audimankar@gmail.com>
2022-01-27 22:03:35 +0530

    Options, ExpandOnlyOnIcon added

--- src/components/tasks/TasksFolderContents.js
+++ src/components/tasks/TasksFolderContents.js
@@ -4,10 +4,85 @@ import SvgIcon from '@mui/material/SvgIcon';
 import { alpha, styled } from '@mui/material/styles';
 import TreeView from '@mui/lab/TreeView';
 import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
+import { useTreeItem } from '@mui/lab';
 import Collapse from '@mui/material/Collapse';
-// web.cjs is required for IE11 support
-// import { useSpring, animated } from 'react-spring/web.cjs';
 import { useSpring, animated } from 'react-spring';
+import { IconButton, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { MoreVert } from '@mui/icons-material';
+import clsx from 'clsx';
+
+const CustomContent = React.forwardRef(function CustomContent(props, ref) {
+  const {
+    classes,
+    className,
+    label,
+    nodeId,
+    icon: iconProp,
+    expansionIcon,
+    displayIcon,
+  } = props;
+
+  const {
+    disabled,
+    expanded,
+    selected,
+    focused,
+    handleExpansion,
+    handleSelection,
+    preventSelection,
+  } = useTreeItem(nodeId);
+
+  const icon = iconProp || expansionIcon || displayIcon;
+
+  const handleMouseDown = (event) => {
+    preventSelection(event);
+  };
+
+  const handleExpansionClick = (event) => {
+    handleExpansion(event);
+  };
+
+  const handleSelectionClick = (event) => {
+    handleSelection(event);
+  };
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className={clsx(className, classes.root, {
+        [classes.expanded]: expanded,
+        [classes.selected]: selected,
+        [classes.focused]: focused,
+        [classes.disabled]: disabled,
+      })}
+      onMouseDown={handleMouseDown}
+      ref={ref}
+    >
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+      <div onClick={handleExpansionClick} className={classes.iconContainer}>
+        {icon}
+      </div>
+      <Typography
+        onClick={handleSelectionClick}
+        component="div"
+        className={classes.label}
+      >
+        {label}
+      </Typography>
+    </div>
+  );
+});
+
+CustomContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  displayIcon: PropTypes.node,
+  expansionIcon: PropTypes.node,
+  icon: PropTypes.node,
+  label: PropTypes.node,
+  nodeId: PropTypes.string.isRequired,
+};
 
 function MinusSquare(props) {
   return (
@@ -21,7 +96,6 @@ function MinusSquare(props) {
 function PlusSquare(props) {
   return (
     <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
     </SvgIcon>
   );
@@ -35,7 +109,6 @@ function CloseSquare(props) {
       style={{ width: 14, height: 14 }}
       {...props}
     >
-      {/* tslint:disable-next-line: max-line-length */}
       <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
     </SvgIcon>
   );
@@ -61,14 +134,11 @@ function TransitionComponent(props) {
 }
 
 TransitionComponent.propTypes = {
-  /**
-   * Show the component; triggers the enter or exit states
-   */
   in: PropTypes.bool,
 };
 
 const StyledTreeItem = styled((props) => (
-  <TreeItem {...props} TransitionComponent={TransitionComponent} sx={{ mt: 1, mb: 1 }}/>
+  <TreeItem {...props} ContentComponent={CustomContent} TransitionComponent={TransitionComponent} />
 ))(({ theme }) => ({
   [`& .${treeItemClasses.iconContainer}`]: {
 		color: theme.palette.primary.main,
@@ -85,11 +155,14 @@ const StyledTreeItem = styled((props) => (
 }));
 
 export default function TasksFolderContents({ data }) {
-	// console.log(data);
+	const sectionOptions = () => {
+		console.log('section clicked');
+	}
+
   return (
     <TreeView
       aria-label="customized"
-			defaultExpanded={['section0', 'task00']}
+			defaultExpanded={['section0']}
       defaultCollapseIcon={<MinusSquare />}
       defaultExpandIcon={<PlusSquare />}
       defaultEndIcon={<CloseSquare />}
@@ -97,37 +170,17 @@ export default function TasksFolderContents({ data }) {
     >
 			{
 				data[0].data.map((section, sectionIndex) => {
-					return <StyledTreeItem key={Math.random()} nodeId={`section${sectionIndex}`} label={section.sectionName}>
+					return <StyledTreeItem key={Math.random()} nodeId={`section${sectionIndex}`} label={<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}><Typography color={'text.secondary'}>{section.sectionName}</Typography><IconButton onClick={sectionOptions}><MoreVert /></IconButton></Box>}>
 						{section.sectionData.map((task, taskIndex) => {
-							return <StyledTreeItem key={Math.random()} nodeId={`task${taskIndex}${sectionIndex}`} label={task.taskName}>
+							return <StyledTreeItem key={Math.random()} nodeId={`task${sectionIndex}${taskIndex}`} label={<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}><Typography color={'text.secondary'}>{task.taskName}</Typography><IconButton onClick={sectionOptions}><MoreVert /></IconButton></Box>}>
 								{task.taskData.map((subtask, subtaskIndex) => {
-									return <StyledTreeItem key={Math.random()} nodeId={`subtask${subtaskIndex}${taskIndex}${sectionIndex}`} label={subtask.subtaskName} />
+									return <StyledTreeItem key={Math.random()} nodeId={`subtask${sectionIndex}${subtaskIndex}${taskIndex}`} label={<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}><Typography color={'text.secondary'}>{subtask.subtaskName}</Typography><IconButton onClick={sectionOptions}><MoreVert /></IconButton></Box>} />
 								})}
 							</StyledTreeItem>
 						})}
 					</StyledTreeItem>
 				})
 			}
-      {/* <StyledTreeItem nodeId="yesterday" label="Yesterday">
-        <StyledTreeItem nodeId="2" label="Task 1">
-					<StyledTreeItem nodeId="3" label="Idk do something">
-						<StyledTreeItem nodeId="4" label="Idk do something even boring" />
-					</StyledTreeItem>
-				</StyledTreeItem>
-      </StyledTreeItem>
-			<StyledTreeItem nodeId="today" label="Today">
-        <StyledTreeItem nodeId="12" label="Task 1">
-					<StyledTreeItem nodeId="13" label="Idk do something even boring" />
-				</StyledTreeItem>
-				<StyledTreeItem nodeId="121" label="Task 2">
-					<StyledTreeItem nodeId="131" label="Idk do something even boringer" />
-				</StyledTreeItem>
-      </StyledTreeItem>
-			<StyledTreeItem nodeId="tomorrow" label="Tomorrow">
-        <StyledTreeItem nodeId="112" label="Task 1">
-					<StyledTreeItem nodeId="113" label="Idk do something even boring" />
-				</StyledTreeItem>
-      </StyledTreeItem> */}
     </TreeView>
   );
 }
